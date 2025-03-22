4a5fe1320110e829fa29fa88a3d684189df7044c
parker <jaypark8282@gmail.com>
2025-03-22 23:40:25 +0900

    ⭐add: 이메일 발송 기능 개발 추가

---
+++ sync-wave-common/src/main/java/com/parker/common/service/EmailService.java
@@ -0,0 +1,61 @@
+package com.parker.common.service;
+
+import com.parker.common.exception.CustomException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Locale;
+
+import static com.parker.common.exception.enums.ResponseErrorCode.FAIL_400;
+import static com.parker.common.exception.enums.ResponseErrorCode.FAIL_500;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final MessageSource messageSource;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Transactional
+    public void sendEmail(String toEmail, String subject, String body) {
+        MimeMessage message = createMimeMessage(toEmail, subject, body);
+        try {
+            mailSender.send(message);
+            log.info("Email successfully sent to {}", toEmail);
+        } catch (MailException e) {
+            log.error("Failed to send email to {}", toEmail, e);
+            throw new CustomException(FAIL_500.code(), messageSource.getMessage("mail.send.fail", null, Locale.getDefault()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private MimeMessage createMimeMessage(String to, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            return message;
+        } catch (MessagingException e) {
+            log.error("Failed to construct email message", e);
+            throw new CustomException(FAIL_500.code(), messageSource.getMessage("mail.message.create.failed", null, Locale.getDefault()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- sync-wave-service/src/test/java/com/parker/service/api/v1/user/service/UserServiceTest.java
+++ sync-wave-service/src/test/java/com/parker/service/api/v1/user/service/UserServiceTest.java
@@ -4,16 +4,23 @@ import com.parker.common.enums.Role;
 import com.parker.common.enums.UserStatus;
 import com.parker.common.jpa.entity.UserEntity;
 import com.parker.common.jpa.repository.UserRepository;
+import com.parker.common.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+
+@Slf4j
 @Transactional
 @SpringBootTest
 class UserServiceTest {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EmailService emailService;
 
     @Test
     void deleteUserInfo() {
@@ -35,5 +42,21 @@ class UserServiceTest {
         userRepository.deleteByEmail(saveResultEntity.getEmail());
 
         Assertions.assertTrue(userRepository.findByEmail(saveResultEntity.getEmail()).isEmpty());
+    }
+
+    @Test
+    void passwordResetEmailSend() {
+        //given
+        String toEmail = "parkdev@kakao.com";
+        String subject = "5월 부터 지원하자!";
+        String body = "가자!!!";
+
+        // when & then
+        try {
+            emailService.sendEmail(toEmail, subject, body);
+            log.info("발송 성공!!");
+        } catch (Exception e) {
+            Assertions.fail("발송 실패");
+        }
     }
 }

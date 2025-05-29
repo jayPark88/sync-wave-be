f71aba4e8f3d95454f8106d2c66e28a82c3a3f73
parker <jaypark8282@gmail.com>
2025-05-30 00:11:52 +0900

    ️🛠️️ modify: 패스워드 reset requestDTO validation 추가

--- sync-wave-service/src/main/java/com/parker/service/api/v1/todos/dto/TodosDto.java
+++ sync-wave-service/src/main/java/com/parker/service/api/v1/todos/dto/TodosDto.java
@@ -17,10 +17,10 @@ public class TodosDto {
     @NotNull(message = "{todo.task.not.null}")
     @Size(min = 1, max = 255, message = "{todo.task.size}")
     private String task;
-    @NotNull(message = "{todo.dueDate.not.null}")
+    @NotNull(message = "{todo.start.date.not.null}")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
-    @NotNull(message = "{todo.dueDate.not.null}")
+    @NotNull(message = "{todo.due.date.not.null}")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dueDate;
     private String status;
